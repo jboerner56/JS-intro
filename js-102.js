19bc4663697ab851247bb51d7981d9d1af5c3c6b
Jason Boerner <jasonboerner@Jasons-MBP.T-mobile.com>
2019-03-04 15:17:03 -0500

    second file for additional exercises

--- js-102.js
+++ js-102.js
@@ -1,6 +1,18 @@
 // function that when given a start and end number, prints each number between. each on a new line
+// for loop
 function printNumbers(start, end){
-    for(let count =0; count <= end; count++){
-        console.log(printNumbers)
+    for(let count = start; count <= end; count++){
+        console.log(count)
     }
 }
+printNumbers(1, 10)
+// while loop
+function printNumbers(start, end){
+    let count = start;
+    while(count <= end){
+        console.log(count)
+    }
+}
+printNumbers(1, 10)
+
+// print square function
