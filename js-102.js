b1fc306adf1f4e040c266f3e8815c33b0ab04473
Jason Boerner <jasonboerner@Jasons-MBP.T-mobile.com>
2019-03-04 15:01:10 -0500

    adds second file for exercises

---
+++ js-102.js
@@ -0,0 +1,6 @@
+// function that when given a start and end number, prints each number between. each on a new line
+function printNumbers(start, end){
+    for(let count =0; count <= end; count++){
+        console.log(printNumbers)
+    }
+}
