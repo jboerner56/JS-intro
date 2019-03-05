e3e15cad9234d26d00a9cf26728065d56a04fe58
Jason Boerner <jasonboerner@Jasons-MBP.T-mobile.com>
2019-03-05 08:02:45 -0500

    adds printSquare function

--- js-102.js
+++ js-102.js
@@ -16,3 +16,22 @@ function printNumbers(start, end){
 printNumbers(1, 10)
 
 // print square function
+// define the function
+function printSquare(size){
+    // variable for printing the square
+    let rect = '';
+    // first for loop, used for the columns
+    for(let i =0; i < size; i++){
+        // second for loop to be used for building rows
+        // inner loop will run however many times "size is"
+        for (let j = 0; j < size; j++){
+            // adds a * up till the counter hits whatever size in used
+            rect += "*"
+        }
+        // adds line break after each time the inner loop runs
+        rect += "\n"
+    }
+    // returns what the nested for loops print out
+    return rect;
+}
+printSquare(5)
