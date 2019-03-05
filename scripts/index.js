dbb37bdf6836fb65adb20d08e173a8a7b62b32a6
Jason Boerner <jasonboerner@Jasons-MBP.T-mobile.com>
2019-03-05 10:55:37 -0500

    adds placeholder function

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
-console.log("hello")
+const leetDictionary = {
+    // keys are strings, same as python,
+    // but quotes are optional in JS
+    A:  4,
+    E:  3,
+    G:  6,
+    I:  1,
+    O:  0,
+    S:  5,
+    T:  7,
+};
+function translate(string){
+    let result = "";
+    return result;
+}
